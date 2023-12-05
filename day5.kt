e7f5370f2f5c5e8a40faaf022ab072430cf685da
miiila <me@milavotradovec.cz>
2023-12-05 11:53:22 +0100

    Day 5 - finish

--- day5.kt
+++ day5.kt
@@ -11,19 +11,18 @@ fun main() {
         exitProcess(0)
     }
     val transformer = { x: String -> x }
-    val input = loadInput(DAY, true, transformer)
+    val input = loadInput(DAY, false, transformer)
 
 //    println(input)
     val (seeds, maps) = parseInput(input)
     println(solvePart1(seeds, maps))
-    solvePart2(seeds, maps)
+    println(solvePart2(seeds, maps))
 }
 
 data class Range(
     val fromStart: BigInteger,
     val fromEnd: BigInteger,
-    val toStart: BigInteger = 0.toBigInteger(),
-    val toEnd: BigInteger = 0.toBigInteger()
+    val offset: BigInteger = 0.toBigInteger()
 )
 
 fun parseInput(input: List<String>): Pair<List<BigInteger>, Map<String, List<Range>>> {
@@ -44,7 +43,7 @@ fun parseInput(input: List<String>): Pair<List<BigInteger>, Map<String, List<Ran
             continue
         }
         val ranges = line.trim().split(" ").map(String::toBigInteger)
-        maps[currentMap]!!.add(Range(ranges[1], ranges[1] + ranges[2], ranges[0], ranges[0] + ranges[2]))
+        maps[currentMap]!!.add(Range(ranges[1], ranges[1] + ranges[2], ranges[1] - ranges[0]))
     }
 
 
@@ -59,10 +58,10 @@ private fun solvePart1(seeds: List<BigInteger>, maps: Map<String, List<Range>>):
 
 private fun lookupSeedInMaps(seed: BigInteger, maps: Map<String, List<Range>>): BigInteger {
     var current = seed
-    for ((key, ranges) in maps) {
+    for ((_, ranges) in maps) {
         for (range in ranges) {
             if (current >= range.fromStart && current < range.fromEnd) {
-                current = range.toStart + (current - range.fromStart)
+                current -= range.offset
                 break
             }
         }
@@ -72,42 +71,43 @@ private fun lookupSeedInMaps(seed: BigInteger, maps: Map<String, List<Range>>): 
 }
 
 private fun lookupSeedInMaps(seed: Range, maps: Map<String, List<Range>>): BigInteger {
-    var current = seed
-    for ((key, ranges) in maps) {
+    var current = listOf(seed)
+    for ((_, ranges) in maps) {
         for (range in ranges) {
-            if (current.fromStart >= range.fromStart && current < range.fromEnd) {
-                current = range.toStart + (current - range.fromStart)
-                break
-            }
+            current = current.map { splitRanges(it, range) }.flatten()
         }
+        current = current.map { Range(it.fromStart - it.offset, it.fromEnd - it.offset) }
     }
-
-    return current
-    return 0.toBigInteger()
+    return (current.minOf { it.fromStart })
 }
 
 // Part 2
-private fun solvePart2(seeds: List<BigInteger>, maps: Map<String, List<Range>>) {
+private fun solvePart2(seeds: List<BigInteger>, maps: Map<String, List<Range>>): BigInteger {
     var i = 0
     val seedsRanges = mutableListOf<Range>()
     while (i < seeds.count()) {
         seedsRanges.add(Range(seeds[i], seeds[i] + seeds[++i]))
         i++
     }
-    val res = seeds.map { lookupSeedInMaps(it, maps) }
+    val res = seedsRanges.map { lookupSeedInMaps(it, maps) }
+    return res.min()
 }
 
 fun splitRanges(first: Range, second: Range): List<Range> {
-    val res = mutableListOf()
+    val res = mutableListOf<Range>()
     // Overlap
-    if (first.fromStart <= second.fromEnd && second.fromStart <= first.fromEnd) {
+    if (first.fromStart < second.fromEnd && second.fromStart < first.fromEnd) {
+        var overlapStart = first.fromStart
+        var overlapEnd = first.fromEnd
         if (first.fromStart < second.fromStart) {
             res.add(Range(first.fromStart, second.fromStart))
+            overlapStart = second.fromStart
         }
         if (first.fromEnd > second.fromEnd) {
             res.add(Range(second.fromEnd, first.fromEnd))
+            overlapEnd = second.fromEnd
         }
-        res.add(Range())
+        res.add(Range(overlapStart, overlapEnd, second.offset))
 
     } else {
         res.add(first)
