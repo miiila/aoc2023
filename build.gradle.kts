a8742a46ba37e30b1f593176839c7eda04359e6b
miiila <me@milavotradovec.cz>
2023-12-12 11:30:46 +0100

    adding gradle for deps

---
+++ build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+    kotlin("jvm") version "1.9.21"
+}
+
+sourceSets {
+    main {
+        kotlin.srcDir("src")
+    }
+}
+
+tasks {
+    wrapper {
+        gradleVersion = "8.5"
+    }
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0-RC")
+}
